4a34765312a05968711876ffbfc0cc58c02683d2
Aka-DoctorCode <37858279+Aka-DoctorCode@users.noreply.github.com>
2026-01-23 09:46:37 -0400

    License change
    License change to all right reserved

--- AscensionCooldownManager.lua
+++ AscensionCooldownManager.lua
@@ -1,3 +1,16 @@
+-------------------------------------------------------------------------------
+-- Project: AscensionCooldownManager
+-- Author: Aka-DoctorCode 
+-- File: AscensionCooldownManager.lua
+-- Version: 12.0.0
+-------------------------------------------------------------------------------
+-- Copyright (c) 2025–2026 Aka-DoctorCode. All Rights Reserved.
+--
+-- This software and its source code are the exclusive property of the author.
+-- No part of this file may be copied, modified, redistributed, or used in 
+-- derivative works without express written permission.
+-------------------------------------------------------------------------------
+---@diagnostic disable: undefined-global
 local ADDON_NAME = "AscensionCooldownManager"
 local ATS = CreateFrame("Frame")
 
--- Core.lua
+++ Core.lua
@@ -1,4 +1,15 @@
--- AscensionCooldownManager (AscensionCooldownManager) - Core.lua
+-------------------------------------------------------------------------------
+-- Project: AscensionCooldownManager
+-- Author: Aka-DoctorCode 
+-- File: Core.lua
+-- Version: 12.0.0
+-------------------------------------------------------------------------------
+-- Copyright (c) 2025–2026 Aka-DoctorCode. All Rights Reserved.
+--
+-- This software and its source code are the exclusive property of the author.
+-- No part of this file may be copied, modified, redistributed, or used in 
+-- derivative works without express written permission.
+-------------------------------------------------------------------------------
 local addonName, AscensionCooldownManager = ...
 local AceAddon = LibStub("AceAddon-3.0")
 local Ascension = AceAddon:NewAddon(AscensionCooldownManager, addonName, "AceConsole-3.0", "AceEvent-3.0")
